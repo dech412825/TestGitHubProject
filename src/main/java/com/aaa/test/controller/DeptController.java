40f641e16ce7ca0f5508ec0e9acba49216abecd4
dech412825 <2364887196@qq.com>
2018-11-29 09:31:16 +0800

    Initial commit

---
+++ src/main/java/com/aaa/test/controller/DeptController.java
@@ -0,0 +1,13 @@
+package com.aaa.test.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:zz
+ * createTime:2018-11-29 09:05
+ */
+public class DeptController {
+    public static void main(String[] args) {
+        System.out.println("测试github 111");
+    }
+}
