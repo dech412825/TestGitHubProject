ee1aa101e3a9a52e29850fc896f9c926ebcc2420
dech412825 <2364887196@qq.com>
2018-11-29 10:46:51 +0800

    加了一个新的class

--- src/main/java/com/aaa/test/controller/DeptController.java
+++ src/main/java/com/aaa/test/controller/DeptController.java
@@ -7,7 +7,10 @@ package com.aaa.test.controller;
  * createTime:2018-11-29 09:05
  */
 public class DeptController {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("测试github 111");
+        System.out.println("测试合并分支");
+
     }
 }
